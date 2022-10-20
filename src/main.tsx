840c6f3b0a850edc9f3697398d9def3fd0623a99
ElenaMLopez <elena@holafly.com>
2022-10-20 12:49:45 +0200

    Install router, bootstrap, initial routes added

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// We need BrowserRouter to wrap our app and have routing
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {/* BrowserRouter wrap our app */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
